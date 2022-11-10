262f525e736a0896850e26687efa561115430699
samuelindberg <samuel.lindberg01@icloud.com>
2022-11-10 14:31:40 +0100

    styling påbörjad, och queryselector hittar iblderna

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 let player1turn = true; 
-let player1 = prompt("Spelare 1 namn:");
-let player2 = prompt("Spelare 2 namn:");
+// let player1 = prompt("Spelare 1 namn:");
+// let player2 = prompt("Spelare 2 namn:");
 let player1Wins = 0;
 let player2Wins = 0;
 let board = [0,0,0,0,0,0,0,0,0];
-
-
-document.getElementById("name1").innerHTML = "player 1: " + player1;
-document.getElementById("name2").innerHTML = "player 2: " + player2;
+let winnerPerson = false;
 let winnerTitle = document.getElementById("winnertag");
+let images1 = document.querySelectorAll("img");
+let images2 = document.getElementsByTagName("img");
+let images3 = document.getElementsByClassName("images");
+let player1box = document.getElementById("player1-shape");
+
+
+
 function winScreen(winner){
     winnerTitle.innerHTML= winner + " vann!";
-    board = [0,0,0,0,0,0,0,0,0];
     winnerTitle.style.transform= "scale(200%)";
-    winnerTitle.style.color="green";
-    
+    winnerTitle.style.color="green"; 
+    winnerPerson=true;
 }
 function checkWin(){ 
     if(board[0]==1 && board[3]==1 && board[6]==1){
         winScreen(player1);
+        images1.style.display="none";
+        images2.style.display="none";
+        images3.style.display="none";
     }
     if(board[0]==2 && board[3]==2 && board[6]==2){
         winScreen(player2);
@@ -62,6 +68,7 @@ function checkWin(){
      if(board[2]==1 && board[4]==1 && board[6]==1){
         winScreen(player1);
      }
+	
 }
 function addSymbol(ruta){
    
@@ -84,4 +91,22 @@ function addSymbol(ruta){
     }
     checkWin();
 }
+function restart(){
+    if(winnerPerson){
+      winnerPerson=false;
+      board = [0,0,0,0,0,0,0,0,0];
+      alert("funkar")
+      alert(board);
+      alert(images1.length);
+    }
+}
+function miniPlayerBox(){
+   player1box.style.height="100px";
+}
+function submitName(){
+   let usernames = [[document.getElementById("names1").value],[document.getElementById("names2").value]];
+   document.getElementById("name1").innerHTML = "player 1: " + usernames[0];
+   document.getElementById("name2").innerHTML = "player 2: " + usernames[1];
+   document.getElementById("insert-name").style.display ="none";
 
+}
