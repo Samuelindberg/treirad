6326b6c4c9ec239e2ccdac9d9923579026631c32
samuelindberg <samuel.lindberg01@icloud.com>
2022-11-12 12:49:30 +0100

    pulerande playknapp och vinstknapp, ändra färg fungerar och knappen är gömd i drop-downen

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let player1turn = true;
-colors = ["white","#FFB9EF","#FF4C4C"];
-let Colorbuttons = [document.getElementById("changecolor2"),document.getElementById("changecolor2")]
+let colors = ["white","#FFCB4C","#5B974E","#FF4C4C","#FFB9EF"];
+
+let playerColors = {
+   colorButtons : [document.getElementById("changecolor2"),document.getElementById("changecolor2")],
+   playerColor: [0,0]
+};
 let player1Wins = 0;
 let player2Wins = 0;
 let playerWinsDisplay = [document.getElementById("p1-wins"),document.getElementById("p2-wins")];
@@ -85,14 +89,13 @@ function checkWin(){
    }
 }
 function addSymbol(ruta){
-
-  
       if(board[ruta-1]==0){
          if(player1turn){
             document.getElementById("cross"+ruta).style.display="block";
             player1turn =false;
             board[ruta-1]=1;
-          } else{document.getElementById("circle"+ruta).style.display="block";player1turn=true;board[ruta-1]=2} 
+          } else{document.getElementById("circle"+ruta).style.display="block";
+          player1turn=true;board[ruta-1]=2} 
       
       } else{
        // ska vara upptagen-ruta 
@@ -133,18 +136,24 @@ function submitName(){
 
 playerBoxes[0].addEventListener("mouseenter",function(){
    playerBoxes[0].style.height="33.3vw";
-
-   document.getElementById("display-symbol1").style.transform="scale(200%)";
+   setTimeout(() => {
+      document.getElementById("changecolor1").style.opacity="1";
+   }, 150);
 
 });
 playerBoxes[0].addEventListener("mouseleave",function(){
   playerBoxes[0].style.height="100px";
+  document.getElementById("changecolor1").style.opacity="0";
 });
 playerBoxes[1].addEventListener("mouseenter",function(){
   playerBoxes[1].style.height="33.3vw";
+  setTimeout(() => {
+   document.getElementById("changecolor2").style.opacity="1";
+}, 150);
 });
 playerBoxes[1].addEventListener("mouseleave",function(){
  playerBoxes[1].style.height="100px";
+ document.getElementById("changecolor2").style.opacity="0";
 });
 document.getElementById("names1").addEventListener("click",function(){
    if(inputOpen[1]){
@@ -168,10 +177,33 @@ document.getElementById("names2").addEventListener("click",function(){
    document.getElementById("names2").style.transform="scale(105%)";
    inputOpen[1]=true;
 });
-// document.getElementById("play").addEventListener("mouseenter",function(){
-//    const playButton = document.getElementById("play");
-//    while(playButton.parentNode.matches(":hover")){
-//       playButton.style.transform="scale(110%)";
-//    }
-//    playButton.style.backgroundColor="black";
-// });
+
+function changeColor(player){
+   
+      if(player==1){
+         document.getElementById("changecolor1").style.color=colors[playerColors.playerColor[0]];
+         for(let i=1; i<=9;i++){
+         document.getElementById("cross"+i).style.color=colors[playerColors.playerColor[0]];
+         }
+         if(playerColors.playerColor[0]!=colors.length-1){
+            playerColors.playerColor[0]++;
+         }
+         else{
+            playerColors.playerColor[0]=0;
+         }
+     
+      } 
+      else{
+         document.getElementById("changecolor2").style.color=colors[playerColors.playerColor[1]];
+         for(let i=1; i<=9;i++){
+            document.getElementById("circle"+i).style.color=colors[playerColors.playerColor[1]];
+            }
+            if(playerColors.playerColor[1]!=colors.length-1){
+               playerColors.playerColor[1]++;
+            }
+            else{
+               playerColors.playerColor[1]=0;
+            }
+      }
+   
+}
